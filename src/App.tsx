39d441497e2722659b8632861e0d5802c1d5bb3b
AVidhanR <itsvidhanreddy@gmail.com>
2025-05-06 22:50:54 +0530

    removed the clutter - simple animation demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { gsap } from "gsap";
 
 function App() {
-  const [count, setCount] = useState(0)
+  gsap.to(".box", {
+    duration: 2,
+    rotation: 360,
+    repeat: 2,
+  });
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="main-div">
+      <h1>gsap animation - using npm.</h1>
+      <div className="box gradient-green"></div>
+      <h2>refresh to see the animation maybe... 👀</h2>
+    </div>
+  );
 }
 
-export default App
+export default App;
